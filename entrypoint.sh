049f46fd7568777040725ff6b81ced5ac54a4d35
Joe Matthew <joe.matthew@bitpanda.com>
2024-07-24 11:48:22 +0200

    fix: try duplicating the data directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,13 +10,15 @@ else
 fi
 
 echo "Starting Anvil with chain id $CHAIN_ID"
-if [ -d /data ]; then
-    echo "Data volume detected, copying static files to /data"
-else
-    mkdir /data
-    echo "No data volume detected, get the configuration from /data"
-fi
-cp -R /data-static/* /data/
+for dir in /data /data2; do
+  if [ -d "$dir" ]; then
+    echo "Data volume detected, copying static files to $dir"
+  else
+    mkdir -p "$dir"
+    echo "No data volume detected, created $dir and copying static files"
+  fi
+  cp -R /data-static/* "$dir/"
+done
 
 # Pass all remaining parameters to anvil
 anvil --load-state anvil-state.json --host "0.0.0.0" --config-out /data/accounts --chain-id "${CHAIN_ID}" "$@"
